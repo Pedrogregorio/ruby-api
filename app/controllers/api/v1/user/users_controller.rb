d13e58fa4c0af19a212bd29020ecd74b7f07522a
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-05-06 11:34:37 -0400

    [update] validações para criar usuario

--- app/controllers/api/v1/user/users_controller.rb
+++ app/controllers/api/v1/user/users_controller.rb
@@ -3,7 +3,16 @@ class Api::V1::User::UsersController < Api::ApiController
   def create
     user = User.create(name: params[:name], email: params[:email], idade: params[:idade])
     if !user.save
-      render json: { error: 'Usuario nao cadastrado' } 
+      render json: { error: "Usuario nao cadastrado #{user.errors.full_messages}" }
+    else
+      render json: user
+    end
+  end
+
+  def show
+    user = User.where(id: params[:id])
+    if user.blank?
+      render json: { error: 'Usuario nao existe' }
     else
       render json: user
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :index, only: [:index]
       namespace :user do
-        resource :user, only: [:create, :destroy]
+        resource :user, only: [:create, :destroy, :show]
       end
     end
   end
